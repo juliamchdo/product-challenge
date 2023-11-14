dcaf5a31b1c5f80244d779be09d3626d8d3b7829
julia <juliamachado226@gmail.com>
2023-11-14 14:38:00 -0300

    changed sidebar items

--- src/components/SidebarItems/index.tsx
+++ src/components/SidebarItems/index.tsx
@@ -1,7 +1,6 @@
 import { ItemCard, ItemsContainer, QuantityButton } from "./styled";
 import { SidebarItemsProps } from "../../types/sidebar-items";
 import { useEffect, useState } from "react";
-import { ProductProps } from "../../types/product";
 import close from "../../assets/close.svg";
 
 export function SidebarItems({
@@ -10,72 +9,79 @@ export function SidebarItems({
   setTotal,
   total,
 }: SidebarItemsProps) {
-  const [price, setPrice] = useState(0);
-  const [totalItems, setTotalItems] = useState(1);
+  const [itemStates, setItemStates] = useState(
+    sidebarItems.map((item) => ({
+      price: Number(item.price),
+      totalItems: 1,
+    }))
+  );
 
   useEffect(() => {
-    const newTotal = sidebarItems.reduce(
-      (acc, item) => acc + Number(item.price),
-      0
-    );
+    const newTotal = itemStates.reduce((acc, item) => acc + item.price, 0);
     setTotal(newTotal);
+  }, [setTotal, itemStates]);
 
-    sidebarItems.map((item) => {
-      setPrice(Number(item.price));
-    });
-  }, [setTotal, sidebarItems]);
-
-  function addItem(item: ProductProps) {
-    setPrice(price + Number(item.price));
-    setTotal(total + Number(item.price));
-    setTotalItems(totalItems + 1);
+  function addItem(index: number) {
+    const newItems = [...itemStates];
+    newItems[index].price += Number(sidebarItems[index].price);
+    newItems[index].totalItems += 1;
+    setItemStates(newItems);
   }
 
-  function removeItem(item: ProductProps) {
-    setPrice(price - Number(item.price));
-    setTotal(total - Number(item.price));
-    setTotalItems(totalItems - 1);
+  function removeItem(index: number) {
+    const newItems = [...itemStates];
+    if (newItems[index].totalItems > 1) {
+      newItems[index].price -= Number(sidebarItems[index].price);
+      newItems[index].totalItems -= 1;
+      setItemStates(newItems);
+    }
   }
 
-  function deleteItem(item: ProductProps){
-    const id = item.id;
-    const newList = sidebarItems.filter((i) => i.id !== id);
-    console.log(newList)
-    setChartItems(newList)
+  function deleteItemFromChart(index: number) {
+    const newItems = [...itemStates];
+    const removedItem = newItems.splice(index, 1)[0];
+    setItemStates(newItems);
+    setChartItems((prevItems) =>
+      prevItems.filter((item) => item.id !== sidebarItems[index].id)
+    );
+    setTotal(total - removedItem.price);
   }
 
   return (
     <ItemsContainer>
-      {sidebarItems.map((item) => {
-        return (
-          <ItemCard key={item.id}>
-            <div className="item-group">
-              <img src={item.photo} alt={item.name} />
-              <p>{item.name}</p>
+      {sidebarItems.map((item, index) => (
+        <ItemCard key={item.id}>
+          <div className="item-group">
+            <img src={item.photo} alt={item.name} />
+            <p>{item.name}</p>
+          </div>
+
+          <QuantityButton>
+            <p>Qtd:</p>
+            <div className="qnt-conatainer">
+              <button
+                className="button-left"
+                disabled={itemStates[index].totalItems === 1}
+                onClick={() => removeItem(index)}
+              >
+                -
+              </button>
+              <span>{itemStates[index].totalItems}</span>
+              <button className="button-right" onClick={() => addItem(index)}>
+                +
+              </button>
             </div>
+          </QuantityButton>
 
-            <QuantityButton>
-              <p>Qtd:</p>
-              <div className="qnt-conatainer">
-                <button
-                  className="button-left"
-                  disabled={totalItems === 1}
-                  onClick={() => removeItem(item)}
-                >
-                  -
-                </button>
-                <span>{totalItems}</span>
-                <button className="button-right" onClick={() => addItem(item)}>
-                  +
-                </button>
-              </div>
-            </QuantityButton>
-
-            <p className="price">R${price}</p>
-            <img className="delete" src={close} alt="delete" onClick={() =>  deleteItem(item)} />
-          </ItemCard>
-        );
-      })}
+          <p className="price">R${itemStates[index].price}</p>
+          <img
+            className="delete"
+            src={close}
+            alt="delete"
+            onClick={() => deleteItemFromChart(index)}
+          />
+        </ItemCard>
+      ))}
     </ItemsContainer>
   );
 }
